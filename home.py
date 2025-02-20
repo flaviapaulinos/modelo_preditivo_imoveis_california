7784a090dcc7720236273918937d9a0d7065a8b7
Flavia BPScht <112699827+flaviabpscht@users.noreply.github.com>
2025-02-20 14:53:12 -0300

    home v2

--- home.py
+++ home.py
@@ -26,25 +26,33 @@ modelo=carregar_modelo()
 
 st.title("Previsão de preços de imóveis")
 
-longitude = st.number_input("Longitude", value=-122.3)#valores aleatorios que estavam no nosso modelo
-latitude = st.number_input("Latitude", value=37.88)
+#a select box precisa receber uma lista
+counties = list(gdf_geo['name'].sort_values())
 
-housing_media_age = st.number_input("Idade do imóvel", value=10)
-total_rooms = st.number_input("Total de Cômodos", value=900)
-total_bedrooms = st.number_input("Idade de quartos", value=100)
-population = st.number_input("População", value=300)
-households = st.number_input("Domicílios", value=100)
+selecionar_condado = st.selectbox("Condado", counties)
 
-median_income = st.slider("Renda média (múltiplos de U$10k)", 0.5, 15.0,4.5, 0.5) #valor mínimo, valor máximo, valor padrão, step
+longitude = gdf_geo.query('name== @selecionar_condado')['longitude'].values
+latitude =  gdf_geo.query('name== @selecionar_condado')['latitude'].values
 
-ocean_proximity = st.selectbox("Proximidade do oceano", df['ocean_proximity'].unique())
+housing_media_age = st.number_input("Idade do imóvel", value=10, min_value=1, max_value=50)
+total_rooms =  gdf_geo.query('name== @selecionar_condado')['total_rooms'].values
+total_bedrooms =  gdf_geo.query('name== @selecionar_condado')['total_bedrooms'].values
+population = gdf_geo.query('name== @selecionar_condado')['population'].values
+households =  gdf_geo.query('name== @selecionar_condado')['households'].values
 
-median_income_cat =  st.number_input("Nível de renda", value=3)
+median_income = st.slider("Renda média (em milhares de US$)", 5.0, 100.0, 45.0, 5.0) 
+#para facilitar para o usuário, vamos multiplicar por 10 (depois precisamos dividir por 10 nas colunas que tem como base essa informação)
+#para não dar erro, tudo float, ou tudo inteiro
 
-rooms_per_household =  st.number_input("Cômodos por domicílio", value=8)
-population_per_household = st.number_input("Pessoas por domicílio", value=2)
-bedrooms_per_room =  st.number_input("Quartos por cômodo", value=0.2)
+ocean_proximity = gdf_geo.query('name== @selecionar_condado')['ocean_proximity'].values
 
+
+bins_income=[0, 1.5, 3, 4.5, 6, np.inf]
+median_income_cat = np.digitize(median_income/10, bins=bins_income)
+
+rooms_per_household =  gdf_geo.query('name== @selecionar_condado')['rooms_per_household'].values
+population_per_household = gdf_geo.query('name== @selecionar_condado')['population_per_household'].values
+bedrooms_per_room =  gdf_geo.query('name== @selecionar_condado')['bedrooms_per_room'].values
 
 
 
@@ -56,7 +64,7 @@ entrada_modelo = {
     'total_bedrooms': total_bedrooms,
     'population': population,
     'households': households,
-    'median_income': median_income,
+    'median_income': median_income/10,
     'ocean_proximity': ocean_proximity,
     'median_income_cat': median_income_cat,
     'rooms_per_household': rooms_per_household,
@@ -64,14 +72,13 @@ entrada_modelo = {
     'bedrooms_per_room': bedrooms_per_room,
 }
 
-df_entrada_modelo = pd.DataFrame(entrada_modelo, index=[0]) #o streamlit pede um index, estamos colocando 0 apenas para atender ao programa
+df_entrada_modelo = pd.DataFrame(entrada_modelo, index=[0]) 
 
 botao_previsao = st.button("Prever preco")
-#retorna um botão booleano.
-#checagem para ver se o botão foi acionado para rodar previsao
+
 if botao_previsao:
     preco = modelo.predict(df_entrada_modelo)
-    st.write(f"Preço previsto: | U${preco[0][0]:.2f}")
+    st.write(f"Preço previsto: | US${preco[0][0]:.2f}")
     
     
     
